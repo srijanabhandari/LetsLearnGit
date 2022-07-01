8f3665e640cf056f5b647608a4bb1c833b038590
tamangnabin <nabinblon4@gmail.com>
2022-07-01 14:53:55 -0500

    adding message in Multiply.java

--- Multiply.java
+++ Multiply.java
@@ -3,6 +3,6 @@ public class{
 	int num1 = 5;
     int num2 =7;
 public static void main(String[]args){
-        system.out.println("the multiple is" + num1*num2);
+        system.out.println("the multiple is" + num1*num2 + "aqbc");
         }
 }
